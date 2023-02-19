da71c719f139490b6c1ce2aadcc68714ad716a47
simboehm <78905660+simboehm@users.noreply.github.com>
2023-02-19 16:27:59 +0100

    Update describe_instances.py

--- 1/describe_instances.py
+++ 1/describe_instances.py
@@ -2,18 +2,15 @@ import boto3
 import os
 
 
-# Create an EC2 client using boto3 (option 1)
+# Create an EC2 client using boto3
 ec2_client = boto3.client("ec2")
-
-# Create an EC2 resource using boto3 (option 2)
-ec2_resource = boto3.resource("ec2")
 
 # Call the describe_instances method on the client object to get information
 # about all instances in the account. The response object contains a dictionary
 # with information about the instances, including their tags and instance types.
 response = ec2_client.describe_instances()
 
-# Loop through the reservations and instances to print the instance ID and AMI ID (option1)
+# Loop through the reservations and instances to print the instance ID and AMI ID
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
         instance_id = instance["InstanceId"]
@@ -25,7 +22,10 @@ for reservation in response["Reservations"]:
 separator = os.linesep + "-" * 30 + os.linesep
 print(separator)
 
-# Loop through all instances in the EC2 resource and print their name and type (option 2)
+# Create an EC2 resource using boto3
+ec2_resource = boto3.resource("ec2")
+
+# Loop through all instances in the EC2 resource and print their name and type
 for instance in ec2_resource.instances.all():
     print(f"Instance name: {instance.tags[0]['Value']}")
     print(f"Instance type: {instance.instance_type}")
